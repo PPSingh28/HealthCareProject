8cb1e50db5e121323d04142eb26eae21dd1a50bb
ppsingh <tech.ppsingh@gmail.com>
2024-02-09 20:49:32 +0530

    repo commit

---
+++ src/main/java/in/nit/pawan/repo/SpecializationRepository.java
@@ -0,0 +1,10 @@
+package in.nit.pawan.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import in.nit.pawan.entity.Specialization;
+
+public interface SpecializationRepository extends JpaRepository<Specialization, Long> {
+	
+
+}
