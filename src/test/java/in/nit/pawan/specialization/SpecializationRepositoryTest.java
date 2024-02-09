803f091307d779835a887cb1a6a593da52d36f05
ppsingh <ppsingh028@gmail.com>
2024-02-10 00:36:29 +0530

    repo commit

--- src/test/java/in/nit/pawan/specialization/SpecializationRepositoryTest.java
+++ src/test/java/in/nit/pawan/specialization/SpecializationRepositoryTest.java
@@ -1,5 +1,35 @@
 package in.nit.pawan.specialization;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+
+import in.nit.pawan.entity.Specialization;
+import in.nit.pawan.repo.SpecializationRepository;
+
+@DataJpaTest(showSql = true)
+@Rollback(false)
+@SpringBootTest
 public class SpecializationRepositoryTest {
+	
+	@Autowired
+	private SpecializationRepository repo;
+	
+	/**
+	 * 1.  Test save operation
+	 */
+	public void testSpecCreate() {
+		Specialization spec = new Specialization(null, "CRDLS", "Cardiologists", "They are expert in heart");
+		spec = repo.save(spec);
+		assertNotNull(spec.getId(), "Spec is not created !");
+		
+	}
+	
+	/**
+	 * 2. Test display all operation
+	 */
 
 }
